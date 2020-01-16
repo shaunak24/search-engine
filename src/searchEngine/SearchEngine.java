16390bf21b63096e0962265c37343a33dfe8e4ba
shaunak24 <shaunak241997@gmail.com>
2020-01-16 23:26:50 +0530

    Preprocessor modified

---
+++ src/searchEngine/SearchEngine.java
@@ -0,0 +1,34 @@
+package searchEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class searchEngine {
+
+	private static void printInorder(TreeNode node) {
+		if (node == null)
+			return;
+
+		printInorder(node.left);
+		System.out.print(node.getData() + " ");
+		printInorder(node.right);
+	}
+
+	public static void main(String[] args) {
+
+		List objectList = DataSource.getPersonData();
+		List fieldList = DataSource.getPersonFields();
+
+		Preprocessor preprocessor = new Preprocessor(objectList, fieldList);
+		Map<String, Attribute> attributeObjectMap = preprocessor.getObjectMap();
+
+//		for (Map.Entry entry : preprocessor.getObjectMap().entrySet()) {
+//			System.out.println(entry.getKey());
+//			Attribute attribute = (Attribute) entry.getValue();
+//			System.out.println(attribute.getAttributeMap());
+//			printInorder(attribute.getNodeRef());
+//		}
+		
+	}
+}
